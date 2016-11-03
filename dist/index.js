77067c3fdc83366aedfb18f6a8f4832e03541f5c
Steve Dignam <steve@dignam.xyz>
2016-11-03 19:45:58 -0400

    Added basic keynav for links

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,75 @@
 'use strict';
 
-/* globals document */
+/* globals document console */
 
-var links = document.querySelectorAll('.link-container > li');
+document.addEventListener('DOMContentLoaded', function () {
+
+    var nav = function nav(selector) {
+        var link = document.querySelector(selector);
+
+        link.classList.add('active');
+
+        return {
+            next: function next() {
+                if (link.nextSibling == null) {
+                    return;
+                }
+                link.classList.remove('active');
+                link = link.nextSibling;
+                link.classList.add('active');
+            },
+            previous: function previous() {
+                if (link.previousSibling == null) {
+                    return;
+                }
+                link.classList.remove('active');
+                link = link.previousSibling;
+                link.classList.add('active');
+            },
+            current: link
+        };
+    };
+
+    var key = {
+        75: 'k',
+        74: 'j',
+        76: 'l',
+        72: 'h',
+        68: 'd',
+        65: 'a',
+        83: 's',
+        70: 'f',
+        78: 'n',
+        79: 'o',
+        80: 'p',
+        191: '/'
+
+    };
+
+    var linksNav = nav('.link-container > li');
+
+    document.addEventListener('keydown', function (event) {
+
+        if (key[event.keyCode] === 'j' || event.ctrlKey && key[event.keyCode] === 'n') {
+            linksNav.next();
+        } else if (key[event.keyCode] === 'k' || event.ctrlKey && key[event.keyCode === 'p']) {
+            linksNav.previous();
+        } else if (key[event.keyCode] === 'd') {
+            console.log('delete');
+        } else if (key[event.keyCode] === 'a') {
+            console.log('archive');
+        } else if (key[event.keyCode] === '/' && event.shiftKey) {
+            console.log('help');
+        } else if (key[event.keyCode] === '/') {
+            console.log('search');
+        } else if (key[event.keyCode] === 'l') {
+            //TODO
+            console.log('switch to right column || wrap around to the left column');
+        } else if (key[event.keyCode] === 'h') {
+            //TODO
+            console.log('switch to left coloumn || wrap around to the right column');
+        }
+        console.log(event.keyCode);
+        console.log(event.ctrlKey);
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
-/* globals document */
+/* globals document console */
 
-const links = document.querySelectorAll('.link-container > li');
+document.addEventListener('DOMContentLoaded', () => {
+
+    const nav = selector => {
+        let link = document.querySelector(selector);
+
+        link.classList.add('active');
+
+        return {
+            next: () => {
+                if (link.nextSibling == null) {
+                    return;
+                }
+                link.classList.remove('active');
+                link = link.nextSibling;
+                link.classList.add('active');
+            },
+            previous: () => {
+                if (link.previousSibling == null) {
+                    return;
+                }
+                link.classList.remove('active');
+                link = link.previousSibling;
+                link.classList.add('active');
+            },
+            current: link
+        };
+    };
+
+    const key = {
+        75: 'k',
+        74: 'j',
+        76: 'l',
+        72: 'h',
+        68: 'd',
+        65: 'a',
+        83: 's',
+        70: 'f',
+        78: 'n',
+        79: 'o',
+        80: 'p',
+        191: '/'
+
+    };
+
+    const linksNav = nav('.link-container > li');
+
+    document.addEventListener('keydown', event => {
+
+        if (key[event.keyCode] === 'j' || (event.ctrlKey && key[event.keyCode] === 'n')) {
+            linksNav.next();
+        } else if (key[event.keyCode] === 'k' || (event.ctrlKey && key[event.keyCode === 'p'])) {
+            linksNav.previous();
+        } else if (key[event.keyCode] === 'd') {
+            console.log('delete');
+        } else if (key[event.keyCode] === 'a') {
+            console.log('archive');
+        } else if (key[event.keyCode] === '/' && event.shiftKey) {
+            console.log('help');
+        } else if (key[event.keyCode] === '/') {
+            console.log('search');
+        } else if (key[event.keyCode] === 'l') {
+            //TODO
+            console.log('switch to right column || wrap around to the left column');
+        } else if (key[event.keyCode] === 'h') {
+            //TODO
+            console.log('switch to left coloumn || wrap around to the right column');
+        }
+        console.log(event.keyCode);
+        console.log(event.ctrlKey);
+
+    });
+
+
+
+
+});
