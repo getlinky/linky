641a0599a99bc779273eb0204797f71001c8062c
Steve Dignam <steve@dignam.xyz>
2016-11-11 23:15:16 -0500

    Improved the interaction of modals and selection
    Now the search box won't keep focus when enabling a modal, and like wise
    for the modals themselves.
    
    The javascript is a bit messy for now.
    
    Also added scss-lint config.

--- src/nav.js
+++ src/nav.js
@@ -2,22 +2,52 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const disable_all_modals = () => {
-        [...document.querySelectorAll('.modal-container')].forEach(l => {
-            l.classList.add('hidden');
-            if (l.tagName === 'input') {
-                l.disabled = true;
-
+    // enable close button for close links in modals
+    // find modals --> find children so we don't have to search upward for the
+    // correct parent element
+    [...document.querySelectorAll('.modal-container')].forEach(el => {
+        // disable modal if someone clicks on the darkend background (.modal-container)
+        el.addEventListener('click', l => {
+            if (l.target.classList.contains('modal-container')) {
+                disable_all_modals();
             }
         });
-        document.querySelector('.link-container').focus();
+        [...el.querySelectorAll('a.close')].forEach(l => {
+            l.addEventListener('click', () => {
+                console.log("put click");
+                disable_all_modals();
+            });
+        });
+    });
+
+        // make add button toggle the add modal
+    document.querySelector('div > a[href="#add"]').addEventListener('click', () => {
+        toggle_add_menu();
+    });
+
+    document.querySelector('div > a[href="#settings"]').addEventListener('click', () => {
+        toggle_settings_menu();
+    });
+
+    const disable_all_modals = () => {
+        console.log('disable_all_modals');
+        [...document.querySelectorAll('.modal-container')].forEach(l => {
+            // hide all of the modals
+            l.classList.add('hidden');
+            // disable all of the inputs in the modals
+            [...document.querySelectorAll('input')].forEach(el => {
+                el.disabled = true;
+            });
+        });
+        // enable search <input>
+        document.querySelector('input.search').disabled = false;
+        document.querySelector('.link-container > li').focus();
     };
 
     const nav = () => {
         let link = document.activeElement;
         return {
             next: () => {
-                console.log(document.activeElement);
                 if (document.activeElement.nextSibling == null) {
                     return;
                 }
@@ -25,11 +55,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 link.focus();
             },
             previous: () => {
-                console.log(document.activeElement);
                 if (document.activeElement.previousSibling == null) {
                     return;
                 }
-
                 link = document.activeElement.previousSibling;
                 link.focus();
             }
@@ -37,11 +65,25 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const toggle_help_menu = () => {
+        toggle_search_input();
         document.querySelector('#help').classList.toggle('hidden');
     };
 
+    const toggle_add_menu = () => {
+        toggle_search_input();
+        const el = document.querySelector('#add');
+        el.classList.toggle('hidden');
+        el.querySelector('input').disabled = false;
+        if (!el.classList.contains('hidden')) {
+            // need slight delay so that the keypresses don't insert into the <input>
+            window.setTimeout(() => el.querySelector('input').focus(), 100);
+        }
+    };
+
     const toggle_settings_menu = () => {
-        document.querySelector('#settings').classList.toggle('hidden');
+        const el = document.querySelector('#settings');
+        toggle_search_input();
+        el.classList.toggle('hidden');
     };
 
     const open_current_link_current_tab = () => {
@@ -53,10 +95,12 @@ document.addEventListener('DOMContentLoaded', () => {
         window.open(url, '_blank');
     };
 
+    const toggle_search_input = () =>
+        document.querySelector('input.search').disabled =
+        !document.querySelector('input.search').disabled;
 
-    const modal_in_use = () => {
-        return document.querySelectorAll('.modal-container:not(.hidden)').length > 0;
-    };
+    const settings_modal_active = () =>
+        !document.querySelector('#settings').classList.contains('hidden');
 
     const links_nav = nav();
 
@@ -81,17 +125,64 @@ document.addEventListener('DOMContentLoaded', () => {
                 27: 'esc',
                 219: ']',
                 51: '3',
-                188: ','
+                188: ',',
+                67: 'c'
             }[event.keyCode];
         };
 
+        // always have esc available
+        if (is_key('esc') || (is_key(']') && event.ctrlKey)) {
+            disable_all_modals();
+        }
+
+
+        // disable keybinds if we are using an <input>
+        if (document.activeElement.tagName.toLowerCase() === 'input') {
+            return;
+        }
+
+        // allow for the `,` to work when settings modal is active so that you
+        // can enable and disable the modal with the same button
+        if (settings_modal_active() && is_key(',')) {
+            toggle_settings_menu();
+            return;
+        }
+        const help_modal_in_use =
+            !document.querySelector('#help').classList.contains('hidden');
+        console.log("shdlkfjsldkf ", help_modal_in_use);
+        if (help_modal_in_use && is_key('/') && event.shiftKey) {
+            toggle_help_menu();
+            return;
+        }
+
+        const modal_in_use =
+            document.querySelectorAll('.modal-container:not(.hidden)').length > 0;
+
+        // don't use keybinds when user is using a modal
+        if (modal_in_use) {
+            return;
+        }
+
+        const search_input_in_use = () =>
+            document.activeElement === document.querySelector('input.search');
+
+        // don't need keybinds if we are editing the search input
+        if (search_input_in_use()) {
+            return;
+        }
 
         if (is_key('j') || is_key('k')) {
+            console.log('key is h or k');
             const ael = document.activeElement.tagName.toLowerCase();
-            if (ael === 'input' || modal_in_use()) {
+            console.log(ael);
+            console.log(ael.previousSibling);
+
+            if (ael === 'input' || modal_in_use) {
+                console.log('active elemtn is the input or modal is in use');
                 // do nothing since the user is editing an input / using a modal
                 return;
             } else if (ael === 'body') {
+                console.log('active element is the body');
                 // focus the first element if the activeElement is the body,
                 // which means the user isn't using an input
                 document.querySelector('.link-container > li').focus();
@@ -99,9 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        if (is_key('esc') || (is_key(']') && event.ctrlKey)) {
-            disable_all_modals();
-        } else if (is_key('j') || (event.ctrlKey && is_key('n'))) {
+        if (is_key('j') || (event.ctrlKey && is_key('n'))) {
             links_nav.next();
         } else if (is_key('k') || (event.ctrlKey && is_key('p'))) {
             links_nav.previous();
@@ -109,6 +198,8 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('delete');
         } else if (is_key('a')) {
             console.log('archive');
+        } else if (is_key('c')) {
+            toggle_add_menu();
         } else if (is_key('/') && event.shiftKey) {
             toggle_help_menu();
         } else if (is_key('/')) {
