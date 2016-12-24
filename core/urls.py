d4e65087f550d681961c74a46bcc70c0c639e88c
Steve Dignam <steve@dignam.xyz>
2016-12-24 14:39:52 -0500

    WIP: adding build tools for vue

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import include, url
-from django.contrib.staticfiles.views import serve
-from django.contrib.auth import views as auth_views
 from rest_framework import routers
 
 from . import views
@@ -13,7 +11,3 @@ router.register(r'links', views.LinkViewSet, base_name='links')
 urlpatterns = [
     url(r'^api/', include(router.urls)),
 ]
-
-# serve /static/index.html as / for dev
-if settings.DEBUG:
-    urlpatterns += url(r'^$', serve, kwargs={'path': 'index.html'}),
