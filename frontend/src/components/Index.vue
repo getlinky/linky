e36d015473416de91b1ca12ad6bb48be8a871a7c
Steve Dignam <steve@dignam.xyz>
2016-12-26 15:26:52 -0500

    WIP: moved main.scss and index.js into vue stuff

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -78,4 +78,318 @@ export default {
         }
     },
 }
-    </script>
+</script>
+
+<style lang='scss'>
+
+
+$font-family: georgia, serif;
+$base-font-size: 18px;
+
+$text-gray: #444;
+$max-width-main-content: 40em;
+
+// background colors
+$sepia: #f8f2e3;
+$grayblack: #3c3c3c;
+$light-gray: #eee;
+
+html {
+  color: $text-gray;
+  font-family: $font-family;
+  font-size: $base-font-size;
+  // background-color: $sepia;
+}
+
+body {
+  // disable default margin of browser
+  margin: 0;
+}
+
+a {
+  color: inherit;
+}
+
+// styling for search <input>
+.search {
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom-style: solid;
+  border-bottom-color: lightgray;
+  width: 60%;
+  outline: none;
+  height: 1.5rem;
+  transition: border 300ms ease-out;
+
+  &::placeholder {
+    font-style: italic;
+
+  }
+
+  &:disabled {
+    color: lightgray;
+    border-radius: 5px;
+    background-image: repeating-linear-gradient(
+    45deg,
+    lightgray,
+    lightgray 10px,
+    white 10px,
+    white 20px
+    );
+  }
+
+  &:focus {
+    border-color: $text-gray;
+  }
+
+}
+
+input {
+  font-size: 1rem;
+  font-family: $font-family;
+
+  &:disabled {
+    color: lightgray;
+    background-color: yellow;
+  }
+}
+
+// container for all of the link sections
+.link-container {
+  list-style-type: none;
+  margin: 0 auto;
+  max-width: $max-width-main-content;
+  overflow-wrap: break-word;
+  padding-top: 0;
+
+  * {
+    margin-bottom: .5rem;
+  }
+
+  // link section
+  li {
+    margin-bottom: 1rem;
+    margin-left: .5rem;
+    margin-right: .5rem;
+    padding-left: 5px;
+    border-left-style: solid;
+    border-left-color: #fff;
+
+    &:focus {
+      outline: none;
+      background-color: lighten($light-gray, 3%);
+      border-left-color: $text-gray;
+    }
+  }
+
+  .link-title {
+    margin-top: 0;
+  }
+
+  .description,
+  .source {
+    font-style: italic;
+    margin-top: 0;
+  }
+}
+
+.main-nav {
+  background: $light-gray;
+
+  a {
+    padding: .5rem .5rem;
+    color: inherit;
+    text-decoration: none;
+
+    &:hover {
+      color: lighten($text-gray, 10%);
+    }
+
+    &:not(:first-child) {
+      margin-left: .5rem;
+    }
+
+  }
+
+  .icon {
+    font-size: 1.5rem;
+  }
+
+  h1 {
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+
+  .title {
+    font-style: italic;
+  }
+
+  .items {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .section {
+    align-items: center;
+    display: flex;
+    font-weight: bold;
+    text-decoration: none;
+
+    // make sure the last link section doesn't hit the edge of the browser
+    &:last-child {
+      margin-right: .5rem;
+    }
+
+    &:first-child {
+      margin-left: .5rem;
+    }
+  }
+
+  .settings-logout-save {
+    font-size: .8rem;
+  }
+
+}
+
+// list, archive, or search
+.list-options {
+  margin: 0 auto;
+  max-width: $max-width-main-content;
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    padding: 1rem .5rem;
+    color: inherit;
+    text-decoration: none;
+    font-weight: bold;
+
+    &:hover {
+      color: lighten($text-gray, 10%);
+    }
+
+    &:not(:first-child) {
+      margin-left: .5rem;
+    }
+  }
+
+  .inactive {
+    color: lighten($text-gray, 30%);
+    font-style: italic;
+
+    &:hover {
+      color: lighten($text-gray, 20%);
+    }
+  }
+
+  .search-option-section {
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .list-archive-option-section {
+    display: flex;
+    align-items: center;
+  }
+
+}
+
+// displays text inside the container & with the .display-on-hover class on hover
+// Usage:
+//   1. Outer container uses the container class
+//   2. Items that should display on hover of container get .display-on-hover
+.display-on-hover {
+  float: right;
+  visibility: hidden;
+}
+
+.display-on-hover-container:hover .display-on-hover {
+  visibility: visible;
+}
+
+.hidden {
+  display: none;
+  height: 0;
+  width: 0;
+}
+
+kbd {
+  background-color: #f7f7f7;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, .2), 0 0 0 2px #fff inset;
+  color: #333;
+  display: inline-block;
+  font-family: sans-serif;
+  font-size: 11px;
+  line-height: 1.4;
+  margin: 0 .1em;
+  padding: .1em .6em;
+  text-shadow: 0 1px 0 #fff;
+}
+
+.modal-container {
+  background: rgba(0, 0, 0, .7);
+  bottom: 0;
+  display: flex;
+  outline: 0;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 10;
+}
+
+.modal-inner {
+  box-sizing: border-box;
+  margin: auto;
+  position: relative;
+  min-width: 320px;
+  width: 50%;
+}
+
+.modal {
+  background-color: $light-gray;
+  border-radius: 5px;
+  padding-top: 0;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  ul {
+    margin: 0;
+    list-style: none;
+    padding: 0;
+  }
+
+  nav {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    h1 {
+      padding: 0;
+    }
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+
+    .close {
+      font-size: 1.5rem;
+    }
+  }
+
+  input {
+    width: 100%;
+    height: 1rem;
+    font-size: 1rem;
+    margin-bottom: .5rem;
+  }
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,19 +5,11 @@ import Vue from 'vue'
 // import App from './App.vue'
 import VueRouter from 'vue-router'
 import Index from './components/Index.vue'
-// import hello from './components/Hello.vue'
 
 Vue.use(VueRouter)
 
-const Foo = { template: '<router-link to="/bar">go to bar</router-link>' }
-const Bar = { template: '<router-link to="/">go to base</router-link>' }
-const Base = { template: '<router-link to="/foo">go to foo</router-link>' }
-
 const routes = [
-  // { path: '/', component: index },
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar },
-  { path: '/', component: Index },
+  { path: '/', component: Index }
 ]
 
 const router = new VueRouter({
@@ -28,3 +20,129 @@ const router = new VueRouter({
 new Vue({
   router
 }).$mount('#app')
+
+const nav = () => {
+  console.log('nav created')
+  let link = document.activeElement
+  return {
+    next: () => {
+      if (document.activeElement.nextElementSibling == null) {
+        return
+      }
+      link = document.activeElement.nextElementSibling
+      link.focus()
+    },
+    previous: () => {
+      if (document.activeElement.previousElementSibling == null) {
+        window.scroll(0, 0)
+        return
+      }
+      link = document.activeElement.previousElementSibling
+      link.focus()
+    }
+  }
+}
+
+const open_current_link_current_tab = () => {
+  window.location.href = document.querySelector('.link-container > li:focus').querySelector('a.source').href
+}
+
+const open_current_link_new_tab = () => {
+  const url = document.querySelector('.link-container > li:focus').querySelector('a.source').href
+  window.open(url, '_blank')
+}
+
+const links_nav = nav()
+
+document.addEventListener('keydown', event => {
+  // takes in `event` from the current scope so it can be called more cleanly
+  const is_key = k => {
+    return k === {
+      75: 'k',
+      74: 'j',
+      76: 'l',
+      72: 'h',
+      68: 'd',
+      65: 'a',
+      83: 's',
+      70: 'f',
+      78: 'n',
+      79: 'o',
+      80: 'p',
+      191: '/',
+      13: 'enter',
+      27: 'esc',
+      219: ']',
+      51: '3',
+      188: ',',
+      67: 'c'
+    }[event.keyCode]
+  }
+
+  // disable keybinds if we are using an <input>
+  if (document.activeElement.tagName.toLowerCase() === 'input') {
+    return
+  }
+
+  const search_input_in_use = () =>
+    document.activeElement === document.querySelector('input.search')
+
+  // don't need keybinds if we are editing the search input
+  if (search_input_in_use()) {
+    return
+  }
+
+  if (is_key('j') || is_key('k')) {
+    console.log('key is h or k')
+    const ael = document.activeElement.tagName.toLowerCase()
+    console.log(ael)
+    console.log(ael.previousSibling)
+
+    if (ael === 'input') {
+      console.log('active elemtn is the input or modal is in use')
+      // do nothing since the user is editing an input / using a modal
+      return
+    } else if (ael === 'body') {
+      console.log('active element is the body')
+      // focus the first element if the activeElement is the body,
+      // which means the user isn't using an input
+      document.querySelector('.link-container > li').focus()
+      return
+    }
+  }
+
+  if (is_key('j') || (event.ctrlKey && is_key('n'))) {
+    links_nav.next()
+  } else if (is_key('k') || (event.ctrlKey && is_key('p'))) {
+    links_nav.previous()
+  } else if (is_key('d')) {
+    console.log('delete')
+  } else if (is_key('a')) {
+    console.log('archive')
+  } else if (is_key('c')) {
+    // TODO: toggle_add_menu()
+    console.log('toggle_add_menu')
+  } else if (is_key('/') && event.shiftKey) {
+    // TODO: toggle_help_menu()
+    console.log('toggle_help_menu')
+  } else if (is_key('/')) {
+    window.setTimeout(() =>
+      document.querySelector('input.search').focus(), 100)
+  } else if (is_key('l')) {
+    document.querySelector('.list-archive-option-section > a.inactive').click()
+  } else if (is_key('h')) {
+    document.querySelector('.list-archive-option-section > a.inactive').click()
+  } else if (is_key('o')) {
+    open_current_link_new_tab()
+  } else if (is_key('enter')) {
+    open_current_link_current_tab()
+  } else if (is_key('3') && event.shiftKey) {
+    console.log('delete current item')
+  } else if (is_key(',')) {
+    // TODO: toggle_settings_menu()
+    console.log('toggle settings menu')
+  }
+
+  console.log(event.keyCode)
+  console.log(event.ctrlKey)
+})
