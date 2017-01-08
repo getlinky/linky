f73ae6c29b8949563d1a8e026fa5f7b9b09b62aa
Christopher Dignam <me@christopher.xyz>
2017-01-08 17:24:15 -0500

    Moved from Session to Token based authentication
    - Updated store.js to support token based auth.
    - Modified settings.py to enable TokenAuthentication

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,11 +9,11 @@ const store = new Vuex.Store({
       user: {
         authenticated: false,
         email: '',
+        token: '',
       },
     links: [],
     errors: {
         addLink: null,
-        updateEmail: [],
         removeLink: [],
         archiveLink: [],
         unarchiveLink: [],
@@ -83,8 +83,10 @@ const store = new Vuex.Store({
       addLinkErrorsClear (state) {
         state.errors.addLink = null
       },
-      loginSuccessful(state) {
+      loginSuccessful(state, token) {
         state.user.authenticated = true
+        state.user.token = token
+        localStorage.setItem('token', token)
         state.errors.login = []
       },
       loginErrors(state, errors) {
@@ -94,6 +96,7 @@ const store = new Vuex.Store({
         state.user.email = ''
         state.user.authenticated = false
         state.links = []
+        localStorage.clear()
       },
       logoutErrors(state, errors) {
         state.errors.logout = errors
@@ -130,13 +133,10 @@ const store = new Vuex.Store({
     login (context, credentials) {
         context.commit('updateEmail', credentials.email)
 
-        axios.post('/rest-auth/login/', credentials, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+        axios.post('/rest-auth/login/', credentials)
         .then(response => {
           console.log('logged in')
-          context.commit('loginSuccessful')
+          context.commit('loginSuccessful', response.data.key)
           // fetch links once user is logged in
           store.dispatch('refreshLinks')
         })
@@ -147,26 +147,34 @@ const store = new Vuex.Store({
 
     },
     isAuthenticated (context, credentials) {
-      axios.get('/api/users/me', {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+      let token = context.state.user.token
+      if (!token) {
+        token = localStorage.getItem('token')
+      }
+      if (!token) {
+        console.warn('No valid token. Cannot authenticate.')
+        context.commit('logout')
+        return
+      }
+      axios.get('/api/users/me',
+        {headers: {'Authorization': 'Token ' + token}})
         .then(response => {
           console.log('is authenticated')
           context.commit('updateEmail', response.data.email)
+          context.commit('loginSuccessful', token)
           context.commit('checkAuthSuccessfull')
         })
         .catch(error => {
           console.warn('not authenticated', error)
+          context.commit('logout')
         })
 
     },
     logout (context) {
         // Note: empty data payload needed for axios to send the headers as headers
-        axios.post('/rest-auth/logout/', {},  {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        }).then(response => {
+        axios.post('/rest-auth/logout/', {},
+          {headers: {'Authorization': 'Token ' + context.state.user.token}})
+        .then(response => {
           console.log('logged out')
           context.commit('logout')
         })
@@ -176,10 +184,8 @@ const store = new Vuex.Store({
         })
     },
     addLink (context, url) {
-      axios.post('/api/links/', {'url': url}, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+      axios.post('/api/links/', {'url': url},
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
         .then(response => {
           console.log('added ' + url)
           context.commit('addLink', response.data)
@@ -190,9 +196,9 @@ const store = new Vuex.Store({
         })
     },
     refreshLinks (context) {
-      axios.get('/api/links.json', {
-        withCredentials: true
-      }).then(response => {
+      axios.get('/api/links.json',
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
+      .then(response => {
         console.log('Refreshed links')
         context.commit('updateLinks', response.data)
       })
@@ -202,10 +208,8 @@ const store = new Vuex.Store({
       })
     },
     archiveLink (context, id) {
-      axios.patch(`/api/links/${id}/`, {'archived': true}, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+      axios.patch(`/api/links/${id}/`, {'archived': true},
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
         .then(response => {
           console.log('archived ' + id)
           context.commit('archiveLink', id)
@@ -216,10 +220,8 @@ const store = new Vuex.Store({
         })
     },
     unarchiveLink (context, id) {
-      axios.patch(`/api/links/${id}/`, {'archived': false}, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+      axios.patch(`/api/links/${id}/`, {'archived': false},
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
         .then(response => {
           console.log('unarchived ' + id)
           context.commit('unarchiveLink', id)
@@ -230,10 +232,8 @@ const store = new Vuex.Store({
         })
     },
     removeLink (context, id) {
-      axios.delete(`/api/links/${id}/`, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')}
-        })
+      axios.delete(`/api/links/${id}/`,
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
         .then(response => {
           console.log('deleted ' + id)
           context.commit('removeLink', id)
@@ -245,10 +245,8 @@ const store = new Vuex.Store({
         })
     },
     changeEmailAddress (context, email) {
-      axios.patch(`/api/users/me/`, {'email': email}, {
-          withCredentials: true,
-          headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-        })
+      axios.patch('/api/users/me/', {'email': email},
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
         .then(response => {
           console.log('email updated ', email)
           context.commit('updateEmail', email)
@@ -261,10 +259,8 @@ const store = new Vuex.Store({
     changeLinkUrl (context, data) {
       let {id, url} = data
       console.log(url)
-      axios.patch(`/api/links/${id}/`, {'url': url}, {
-        withCredentials: true,
-        headers: {'X-CSRFToken': document.cookie.replace(/^.*=/, '')},
-      })
+      axios.patch(`/api/links/${id}/`, {'url': url},
+        {headers: {'Authorization': 'Token ' + context.state.user.token}})
       .then(response => {
         console.log('updated_url', url)
         context.commit('updateLinkUrl', id)
