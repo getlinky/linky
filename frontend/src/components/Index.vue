018114df58fe8e3be4d0981e3ce0547400f3f2f0
Christopher Dignam <me@christopher.xyz>
2017-01-12 01:31:02 -0500

    Added content to index page
    - Copy about how great this product is
    - Registration fields

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -8,12 +8,44 @@
     </template>
     <router-link v-else to="/login">Login</router-link>
   </linky-nav>
+    <h1><i>Linky</i> — Save links for later</h1>
+    <div class="container">
+      <div class="item">
+        <h2>Why choose this?</h2>
+        <ul>
+          <li>Use simple keybinds to breeze through your lists.</li>
+          <li>A no fuss design enables quick access to what you need.</li>
+          <li>Copy and paste links into your queue like you copy urls.</li>
+          <li>Open Source</li>
+        </ul>
+      </div>
+      <div class="item">
+        <h2>Register Now</h1>
+        <form @submit.prevent="registerUser">
+          <label>Email<br>
+            <input type="email" v-model="email" v-focus="focusEmail" @blur="focusEmail = false" @focus="focusEmail = true" required>
+          </label>
+          <br>
+          <label>Password<br>
+            <input type="password" v-model="password1" required>
+          </label>
+          <br>
+          <label>Password Again<br>
+            <input type="password" v-model="password2" required>
+          </label>
+          <br>
+          <input type="submit" value="Register">
+        </form>
+      </div>
+    </div>
   <notification-test v-if="debug"></notification-test>
   <linkyNotification></linkyNotification>
 </div>
 </template>
 
 <script>
+import { focus } from 'vue-focus'
+import axios from 'axios'
 import linkyNav from './LinkyNav.vue'
 import notificationTest from './notificationTest.vue'
 import linkyNotification from './linkyNotification.vue'
@@ -31,6 +63,17 @@ export default {
       return this.$store.state.background
     },
   },
+  directives: {
+    focus,
+  },
+  data () {
+    return {
+      email: '',
+      password1: '',
+      password2: '',
+      focusEmail: true,
+    }
+  },
   components: {
     linkyNav,
     notificationTest,
@@ -39,7 +82,24 @@ export default {
   methods: {
     logoutUser () {
       this.$store.dispatch('logout')
-        .then(this.$router.replace('/'))
+    },
+    registerUser () {
+      const data = {
+        'email': this.email,
+        'password1': this.password1,
+        'password2': this.password2,
+      }
+      axios.post('/rest-auth/registration/', data)
+      .then(response => {
+        this.$store.commit('notify', {'message': 'Registration Successful', 'level': 'success'})
+        console.info('Registration successful')
+        this.$store.commit('loginSuccessful', response.data.key)
+        this.$router.replace('/list')
+      })
+      .catch(error => {
+        console.warn('Problem registering user.', error)
+        this.$store.commit('notify', {'message': 'Problem Registering User', 'level': 'warning'})
+      })
     },
   },
   mounted () {
@@ -48,3 +108,36 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 2em;
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  .item {
+    flex-basis: max-content;
+    padding-left: 20px;
+    padding-right: 20px;
+    max-width: 400px;
+    font-size: 1.25em;
+
+    h2 {
+      font-size: 1.2em;
+      margin: 0;
+    }
+
+    form {
+      font-weight: normal;
+      padding-top: 1em;
+      font-size: .8em;
+    }
+  }
+}
+</style>
